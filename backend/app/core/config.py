5c04153612fac17af7b73e790bb1cb8aa9845e3f
David Scheiermann <dscheiermann@layer2.de>
2023-09-07 11:27:28 +0200

    change ip to localhost

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7   # 7 days
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
-        "http://85.215.190.209:3000"
+        "http://localhost:3000"
     ]
     PROJECT_NAME: str = "TODOLIST"
     
