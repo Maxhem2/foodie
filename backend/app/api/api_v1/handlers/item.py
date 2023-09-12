2ef4be5ef482c9e3f3b4fdb97052824ed0387cbb
David Scheiermann <dscheiermann@layer2.de>
2023-09-12 11:43:29 +0200

    some random python bug

--- backend/app/api/api_v1/handlers/item.py
+++ backend/app/api/api_v1/handlers/item.py
@@ -12,7 +12,7 @@ item_router = APIRouter()
 
 @item_router.get('/', summary="Get all items of the user", response_model=List[ItemOut])
 async def list(current_user: User = Depends(get_current_user)):
-     items = await ItemService.list_items(current_user)
+   items = await ItemService.list_items(current_user)
 
    for list_items in items:
     afd = list_items.item_id
