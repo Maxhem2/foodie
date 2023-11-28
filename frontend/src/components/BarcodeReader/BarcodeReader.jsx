d5410dde7c946930e04d44e2addf7ad165a2223e
David Scheiermann <dscheiermann@layer2.de>
2023-11-28 22:40:00 +0100

    add BarcodeReader

---
+++ frontend/src/components/BarcodeReader/BarcodeReader.jsx
@@ -0,0 +1,34 @@
+import { useState, useRef, useEffect } from "react";
+import { BrowserMultiFormatReader } from "@zxing/library";
+
+export const BarcodeReader = ({ onResult = (data) => data, onError = () => {} }) => {
+    const videoRef = useRef(null);
+    const [barcodeData, setBarcodeData] = useState();
+    const reader = useRef(new BrowserMultiFormatReader());
+
+    useEffect(() => {
+        if (!barcodeData) {
+            if (!videoRef.current) return;
+            reader.current.decodeFromConstraints(
+                {
+                    audio: false,
+                    video: {
+                        facingMode: "environment",
+                    },
+                },
+                videoRef.current,
+                (result, error) => {
+                    if (result) setBarcodeData(result);
+                    if (error) console.log(error);
+                }
+            );
+            return () => {
+                reader.current.reset();
+            };
+        } else {
+            onResult(barcodeData);
+        }
+    }, [barcodeData, onResult, videoRef]);
+
+    return <video ref={videoRef} />;
+};
